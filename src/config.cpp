77e95860080df0d9c68c47731cab473cde025888
VuqarAhadli <vuqarahadli17@gmail.com>
2025-12-10 00:43:53 +0400

    Implement core functionalities: add command handling, filesystem operations, and WiFi connectivity

---
+++ src/config.cpp
@@ -0,0 +1,9 @@
+#include "config.h"
+
+
+const char* WIFI_SSID = "Fill it";
+const char* WIFI_PASS = "Fill it";
+const char* NTP_SERVER = "pool.ntp.org";
+const long GMT_OFFSET = 4 * 3600;
+const int DAYLIGHT_OFFSET = 0;
+const char* OS_VERSION = "MiniOS-ESP v1.0";
