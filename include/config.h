fbcac2e64df4db106967c3395bbe72eceafee4c0
Vuq17 <vuqarahadli17@gmail.com>
2026-01-11 05:23:53 +0400

    Fix config.h

---
+++ include/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern const char* WIFI_SSID;
+extern const char* WIFI_PASS;
+extern const char* NTP_SERVER;
+extern const long GMT_OFFSET;
+extern const int DAYLIGHT_OFFSET;
+extern const char* OS_VERSION;
+
+#endif 
