209a04dbb8cb6cf084887d3a7e79cdb2858ae161
Vuq17 <vuqarahadli17@gmail.com>
2026-01-11 18:32:28 +0400

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,6 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define TFT_CS   5
+#define TFT_DC   2  // Display Pins for ST7789 review README.me for full wiring setup
+#define TFT_RST  4
+
+
 extern const char* WIFI_SSID;
 extern const char* WIFI_PASS;
 extern const char* NTP_SERVER;
@@ -8,4 +13,4 @@ extern const long GMT_OFFSET;
 extern const int DAYLIGHT_OFFSET;
 extern const char* OS_VERSION;
 
-#endif 
+#endif
